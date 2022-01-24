5dcf3065b0625582e30cb347c04dc6fc2f53ad90
Masahito Osako <velonica.gale2227@gmail.com>
2022-01-25 00:05:55 +0900

    replace to src/

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 
-import theme from '../lib/theme';
-import Fonts from '../lib/font';
+import theme from '../src/lib/theme';
+import Fonts from '../src/lib/font';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
