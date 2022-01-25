e79be88ec72bb03f79beb12720a9bda2adae2bf9
Masahito Osako <velonica.gale2227@gmail.com>
2022-01-25 10:59:43 +0900

    add contentful client

---
+++ src/lib/contentful_client.ts
@@ -0,0 +1,8 @@
+import * as contentful from 'contentful'
+
+// TODO: add environments
+const contentfulClient = contentful.createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE || '',
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_API_KEY || ''
+})
+export default contentfulClient
