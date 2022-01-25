09aa262fe861f4fbf48aa8a37d3a3d45ce8ef655
Masahito Osako <velonica.gale2227@gmail.com>
2022-01-25 19:08:57 +0900

    fix design bug

--- src/components/home/profile.tsx
+++ src/components/home/profile.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 import { Flex, Box, Heading, Text, Avatar, useBreakpointValue } from '@chakra-ui/react'
 
 const Profile: React.FC = () => {
-  const avatarSize = useBreakpointValue({ base: 'xl', md: '2xl' })
+  // NOTE: next.jsが一番最初のrenderingのとき、useBreakpointValueはundefinedを返す。そのため、defaultの値を指定する必要がある
+  const avatarSize = useBreakpointValue({ base: 'xl', md: '2xl' }) || '2xl'
 
   return (
     <Flex justifyContent='space-between' alignItems='flex-start' flexDirection='row' px={{ base: 0, md: 5 }} py={2}>
