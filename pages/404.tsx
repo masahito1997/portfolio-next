17a9c7173ac397bd6c96e0d9402a478bfd801ace
Masahito Osako <velonica.gale2227@gmail.com>
2022-01-31 20:53:24 +0900

    add 404

---
+++ pages/404.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Heading, Box, Text } from '@chakra-ui/react'
+
+const customom_404: React.FC = () => {
+  return (
+    <Box>
+      <Heading as='h2' size='4xl' textAlign='center' mb={2}>404</Heading>
+      <Heading as='h3' size='lg' textAlign='center' mb={6}>Page Not Found</Heading>
+      <Text textAlign='center'>ページが見つかりませんでした。</Text>
+      <Text textAlign='center'>URLが変更・削除されたか、現在ご利用できない可能性があります。</Text>
+    </Box>
+  )
+}
+export default customom_404
