436624681ed2be0fae65681d4224faf2a191a879
Masahito Osako <velonica.gale2227@gmail.com>
2022-01-31 20:58:59 +0900

    add 500 page

---
+++ pages/500.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Box, Heading } from '@chakra-ui/react'
+
+const custom500: React.FC = () => {
+  return (
+    <Box>
+      <Heading as='h2' size='4xl' textAlign='center' mb={2}>500</Heading>
+      <Heading as='h3' size='lg' textAlign='center' mb={6}>Opps, Something is wrong</Heading>
+    </Box>
+  )
+}
+export default custom500
