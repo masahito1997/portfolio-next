ed48e8bef84099a556c2452ccf162f7e748afc33
Masahito Osako <masahito.osako@hey.com>
2023-12-24 23:52:25 +0900

    replace chakra-ui to tailwindcss in header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-import { Box, Heading, Stack, useColorModeValue, Button, Menu, MenuButton, MenuList, MenuItem, IconButton, useColorMode, Link as ChakraLink } from '@chakra-ui/react'
-import { HamburgerIcon, SunIcon, MoonIcon } from '@chakra-ui/icons'
 import Link from 'next/link'
 
 type HeaderLinkProps = {
@@ -10,42 +8,49 @@ type HeaderLinkProps = {
   target?: string
 }
 const HeaderLink: React.FC<HeaderLinkProps> = ({ href, target = '_self', children }) => {
-  return <ChakraLink href={href} p={2} isExternal={target === '_blank'}>{children}</ChakraLink>
+  return <Link href={href} passHref={target === '_blank'} className='p-2 hover:underline'>{children}</Link>
 }
 
 const Header: React.FC = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const clickMenuHandler = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
 
   return (
-    <Box as='header' w='100%' bg={useColorModeValue('#ffffff40', 'gray.800')} >
-      <Box as='nav' w='100%' px={{ base: 0, md: 8 }} h='auto' display='flex' flexDirection={{ base: 'row-reverse', md: 'row' }} justifyContent='flex-start'>
-        <Stack direction={{ base: 'column', md: 'row' }} display={{ base: 'none', md: 'flex' }} justifyContent='end' alignItems='center' flexGrow={1}>
+    <header className='w-full bg-dark'>
+      <nav className='w-full px-0 md:px-4 h-auto flex flex-row-reverse md:flex-row justify-start'>
+        <div className='hidden md:flex justify-end items-center flex-grow'>
           <HeaderLink href='/'>TOP</HeaderLink>
           <HeaderLink href='/works'>Works</HeaderLink>
           <HeaderLink href='/posts'>Blog</HeaderLink>
           <HeaderLink href='/resume'>Resume</HeaderLink>
           <HeaderLink href='https://github.com/m11o' target='_blank'>Github</HeaderLink>
-        </Stack>
-        <Box display={{ base: 'block', md: 'none' }}>
-          <Menu isLazy>
-            <MenuButton icon={<HamburgerIcon />} as={IconButton} variant='outline' />
-            <MenuList display='flex' justifyContent='flex-end' flexDirection='column'>
-              <MenuItem as={HeaderLink} href='/'>TOP</MenuItem>
-              <MenuItem as={HeaderLink} href='/works'>Works</MenuItem>
-              <MenuItem as={HeaderLink} href='/posts'>Blogs</MenuItem>
-              <MenuItem as={HeaderLink} href='/resume'>Resume</MenuItem>
-              <MenuItem as={HeaderLink} href='https://github.com/m11o' target='_blank'>Github</MenuItem>
-            </MenuList>
-          </Menu>
-        </Box>
-        <Button onClick={toggleColorMode} ml={{ base: 0, md: 2 }} mr={{ base: 2, md: 0 }}>
-          {colorMode == 'dark' ? <MoonIcon /> : <SunIcon />}
-        </Button>
-      </Box>
-      <Box>
-        <Heading as='h1' textAlign='center' mt={{ base: 20, md: 40 }} mb={20} fontFamily='header' fontWeight='bold'>Love Beautiful Code</Heading>
-      </Box>
-    </Box>
+        </div>
+        <div className='block md:hidden relative'>
+          <button onClick={clickMenuHandler}
+                  className='flex justify-center items-center border rounded-md border-gray-600 p-2'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                 stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+            </svg>
+          </button>
+          <div
+            className={`${isMenuOpen ? 'flex' : 'hidden'} justify-end flex-col absolute top-12 right-0 w-48 border border-gray-500 rounded-lg bg-gray-700 p-2 z-50 drop-shadow-lg`}>
+            <HeaderLink href='/'>TOP</HeaderLink>
+            <HeaderLink href='/works'>Works</HeaderLink>
+            <HeaderLink href='/posts'>Blog</HeaderLink>
+            <HeaderLink href='/resume'>Resume</HeaderLink>
+            <HeaderLink href='https://github.com/m11o' target='_blank'>Github</HeaderLink>
+          </div>
+        </div>
+      </nav>
+      <div>
+        <h1 className='text-center mt-20 md:mt-40 mb-20 font-header font-bold text-3xl md:text-4xl'>
+          Love Beautiful Code
+        </h1>
+      </div>
+    </header>
   )
 }
 
