0d766b61fbb46c0b8ec3a3dd82d52cef77f8557f
Masahito Osako <masahito.osako@hey.com>
2023-12-26 00:20:40 +0900

    replace from chakra-ui to tailwindcss in pages/posts/index.tsx

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Text, LinkBox, Heading, Flex, Box, Tag } from '@chakra-ui/react'
 import Link from 'next/link'
 
 import { EntryCollection, EntryFields } from 'contentful'
@@ -20,21 +19,18 @@ type blogCardProps = {
 const BlogCard: React.FC<blogCardProps> = ({ id, title, tags, description='', updatedAt }) => {
   const updatedAtDate = new Date(updatedAt)
   return (
-    <LinkBox as='article' p={5} borderBottomWidth='1px' _hover={{
-      borderWidth: '1px',
-      rounded: 'md'
-    }}>
-      <Heading size='lg' as='h3' mb={3}>
-        <Link href={`/posts/${id}`}>{title}</Link>
-      </Heading>
-      <Text fontSize='md' mb={2} noOfLines={[5, 3]}>{description}</Text>
-      <Flex direction='row' justifyContent='space-between' alignItems='flex-end'>
-        <Box>
-          {tags.length ? tags.map((tag) => <Tag key={tag} mx={1} my={1}>{tag}</Tag>) : ''}
-        </Box>
-        <Text fontSize='sm' fontWeight='bold' px={4}>{updatedAtDate.toLocaleDateString('ja-JP')}</Text>
-      </Flex>
-    </LinkBox>
+    <article className='p-5 border-b relative hover:bg-gray-700/40'>
+      <h3 className='text-2xl mb-3'>
+        <Link href={`/posts/${id}`} className='before:block before:absolute before:top-0 before:left-0 before:w-full before:h-full before:z-0'>{title}</Link>
+      </h3>
+      <p className='text-md line-clamp-5 md:line-clamp-3'>{description}</p>
+      <div className='flex justify-between items-baseline mt-4'>
+        <div>
+          {tags.length && tags.map((tag) => <span key={tag} className='rounded bg-gray-400/20 text-gray-300 px-2 py-1 text-xs mr-2 mb-1'>{tag}</span>)}
+        </div>
+        <p className='text-sm font-bold px-4'>{updatedAtDate.toLocaleDateString('ja-JP')}</p>
+      </div>
+    </article>
   )
 }
 
@@ -54,12 +50,12 @@ const BlogList: React.FC<blogListType> = ({ posts, total, currentPage, limit }) 
             const { id, title, description, updatedAt, tags } = post
             return <BlogCard key={id} id={id} title={title} description={description} tags={tags} updatedAt={updatedAt} />
           })}
-          <Box mt={8}>
+          <div className='mt-8'>
             <Paginator path='/posts' total={total} currentPage={currentPage} limit={limit} />
-          </Box>
+          </div>
         </>
       ) : (
-        <Text fontSize='sm' color='gray.400' textAlign='center'>現在、表示できる記事が存在しません</Text>
+        <p className='text-gray-400 text-center text-md'>現在、表示できる記事が存在しません</p>
       )}
     </>
   )
