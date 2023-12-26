811235543f16a795f3b4b9d65850954a45710e36
Masahito Osako <masahito.osako@hey.com>
2023-12-26 11:12:49 +0900

    replace from chakra-ui to tailwindcss in pages/posts/[id].tsx

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import {Heading, Flex, Tag, Text, Box, Code, OrderedList, ListItem, Table} from '@chakra-ui/react'
 
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown, { Components } from 'react-markdown'
 import gfm from 'remark-gfm'
-import ChakraUIRenderer from 'chakra-ui-markdown-renderer'
 import { Entry, EntryFields } from 'contentful'
-import SyntaxHighlighter from 'react-syntax-highlighter'
+import {Prism, SyntaxHighlighterProps} from 'react-syntax-highlighter';
+
 import { atomOneDark } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
 import { GetServerSideProps } from 'next'
 
@@ -21,28 +20,58 @@ type blogDetailProps = {
   markdown: string,
   updatedAt: string
 }
+
+const SyntaxHighlighter = (Prism as any) as React.FC<SyntaxHighlighterProps>;
+
 const BlogDetail: React.FC<blogDetailProps> = ({ title, description, tags, markdown, updatedAt }) => {
-  const customMarkdownTheme = {
+  const customMarkdownTheme: Components = {
     ...markdownTheme,
-    p: (props: any) => <Text mb={4}>{props.children}</Text>,
-    ol: (props: any) => <OrderedList mb={4}>{props.children}</OrderedList>,
-    li: (props: any) => <ListItem mb={2}>{props.children}</ListItem>,
-    table: (props: any) => <Table mb={4}>{props.children}</Table>
+    p: (props: any) => <p className='mb-4'>{props.children}</p>,
+    ol: (props: any) => <ol className='list-decimal mb-4 ml-6'>{props.children}</ol>,
+    ul: (props: any) => <ul className='list-disc mb-4 ml-6'>{props.children}</ul>,
+    li: (props: any) => <li className='mb-2'>{props.children}</li>,
+    table: (props: any) => <table className='mb-4'>{props.children}</table>,
+    th: (props: any) => <th className='py-2 px-4 text-left border'>{props.children}</th>,
+    td: (props: any) => <td className='py-2 px-4 border'>{props.children}</td>,
+    blockquote: (props: any) => {
+      return <blockquote className='mb-4 italic font-bold bg-slate-700 px-4 pt-4 pb-1.5 border-l-4 border-slate-300'>{props.children}</blockquote>
+    },
+    code: (props: any) => {
+      const {children, className, node, ...rest} = props
+      const match = /language-(\w+)/.exec(className || '')
+
+      return match ? (
+        <SyntaxHighlighter
+          {...rest}
+          PreTag="div"
+          language={match[1]}
+          style={atomOneDark}
+          showLineNumbers
+          className='mb-4'
+        >
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+      ) : (
+        <code {...rest} className={`${className} rounded-md bg-red-400/20 text-red-300 px-2.5 py-0.5 text-sm font-bold text-nowrap`}>
+          {children}
+        </code>
+      )
+    }
   }
 
   return (
     <>
       <HeadContent title={`${title} - Love Beautiful Code`} description={description} keywords={tags} />
-      <Box mb={{ base: 10, md: 20 }}>
-        <Heading as='h1' size='xl' mb={{ base: 2, md: 4 }}>{title}</Heading>
-        <Flex justifyContent='space-between' alignItems='flex-end'>
-          <Box>
-            {tags.length ? (tags.map((tag) => <Tag key={tag} mx={1} my={1}>{tag}</Tag>)) : ''}
-          </Box>
-          <Text fontWeight='bold'>{updatedAt}</Text>
-        </Flex>
-      </Box>
-      <ReactMarkdown components={ChakraUIRenderer(customMarkdownTheme)} remarkPlugins={[gfm]}>{markdown}</ReactMarkdown>
+      <div className='mb-10 md:mb-20'>
+        <h1 className='text-3xl mb-2 md:mb-4'>{title}</h1>
+        <div className='flex justify-between items-baseline'>
+          <div>
+            {tags.length ? (tags.map((tag) => <span key={tag} className='rounded bg-gray-400/20 text-gray-300 px-2 py-1 text-xs mr-2 mb-1'>{tag}</span>)) : ''}
+          </div>
+          <p className='font-bold'>{updatedAt}</p>
+        </div>
+      </div>
+      <ReactMarkdown components={customMarkdownTheme} remarkPlugins={[gfm]}>{markdown}</ReactMarkdown>
     </>
   )
 }
