faf1030b938d08be709e8e497b9c9871c1077cbf
Masahito Osako <masahito.osako@hey.com>
2023-12-29 18:29:07 +0900

    Add Header component in root, non-found and error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,13 +1,17 @@
 'use client'
 
 import React from 'react'
+import Header from "../src/components/header";
 
 const custom500: React.FC = () => {
   return (
-    <div>
-      <h2 className='text-6xl text-center mb-2'>500</h2>
-      <h3 className='text-xl text-center mb-6'>Opps, Something is wrong</h3>
-    </div>
+    <>
+      <Header />
+      <div>
+        <h2 className='text-6xl text-center mb-2'>500</h2>
+        <h3 className='text-xl text-center mb-6'>Opps, Something is wrong</h3>
+      </div>
+    </>
   )
 }
 export default custom500
