1c0a94ecc3669880b0d4710161066444b7af8d1b
Masahito Osako <masahito.osako@hey.com>
2023-12-29 18:46:05 +0900

    responseを返すように修正

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -3,4 +3,5 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
   res.setHeader('WWW-authenticate', 'Basic realm="Secure Area"')
   res.status(401)
+  return res.json({ message: 'Basic Authentication Required' })
 }
